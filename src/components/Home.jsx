339a5396fa7bedcc16e8f70244f705450367afab
akprinciple <akeemolayiwola9@gmail.com>
2025-10-21 23:51:44 +0100

    Suspense

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import React, { lazy, Suspense, } from 'react'
-import TopBar from './TopBar'
-import Banner from './Banner'
-import Products from './Products'
-import Category from './Category'
+// import TopBar from './TopBar'
+// import Banner from './Banner'
+// import Products from './Products'
+// import Category from './Category'
 
+const TopBar = lazy(() => import('./TopBar'));
+const Banner = lazy(() => import('./Banner'));
+const Products = lazy(() => import('./Products'));
+const Category = lazy(() => import('./Category'));
 const Home = () => {
-  const TopBar = lazy(() => import('./TopBar'));
-  const Banner = lazy(() => import('./Banner'));
-  const Products = lazy(() => import('./Products'));
-  const Category = lazy(() => import('./Category'));
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className='text-center'>Loading...</div>}>
     <div>
         <TopBar />
         <Banner />
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,12 +4,12 @@ import { Link, Outlet } from 'react-router-dom'
 const Root = () => {
   return (
     <div>
-        <div className='bg-amber-100 text-dark p-3 text-center  font-bold'>
+        <div className='bg-amber-50 text-dark p-3 text-center  font-bold'>
             <Link to={'/'} className='p-3 rounded bg-amber-400'>School</Link>
             <Link to={'/home'} className='mx-4 p-3 rounded bg-amber-500'>Home</Link>
 
         </div>
-        <marquee behavior="" direction="" className='text-6xl bg-amber-50 p-2'>
+        <marquee behavior="" direction="" className='text-6xl p-2'>
             <h1>Welcome</h1>
         </marquee>
         <Outlet/>
