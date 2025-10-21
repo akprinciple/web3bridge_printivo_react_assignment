5ead9e099ec90580338358edc9540523b3153046
akprinciple <akeemolayiwola9@gmail.com>
2025-10-22 00:56:21 +0100

    blog fetch

--- src/components/AxiosTesting.jsx
+++ src/components/AxiosTesting.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const AxiosTesting = () => {
     const [post, setpost] = useState([])
@@ -7,21 +8,31 @@ const AxiosTesting = () => {
     const fetchdata = async() =>{
         try {
             const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            console.log(res.data);
+            
             setpost(res.data)
         } catch (error) {
-            setloading
+            setloading(false)
+            console.log('Error fetching data', error);
         }finally{
             setloading(false)
         }
     }
+    useEffect(() =>{
+        fetchdata()
+    }, [])
   return (
     <div>
-        {loading ? <h1>Loading...</h1> : post.map((row, i) =>{
-            <div key={i}>
-                <h3>{row.title}</h3>
+        {loading ? <h1 className='text-center'>Loading...</h1> : ''}
+        <div className='flex flex-wrap justify-between'>
+
+        { post?.map((row, i)  =>(
+            <div key={i} className='p-3 w-1/3'>
+                <h1 className='text-2xl'>{row.title}</h1>
                 <p>{row.body}</p>
             </div>
-        })}
+        ))}
+        </div>
     </div>
   )
 }
